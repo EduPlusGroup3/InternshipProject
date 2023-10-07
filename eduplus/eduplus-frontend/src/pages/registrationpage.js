3c2cf85253eb5bf6d3ed4fb4129a134cde766641
Rohit Kumar <me.kr.rohit@gmail.com>
2023-10-06 20:21:21 -0400

    Added min age validation for new user

--- eduplus/eduplus-frontend/src/pages/registrationpage.js
+++ eduplus/eduplus-frontend/src/pages/registrationpage.js
@@ -38,12 +38,27 @@ const RegistrationPage = () => {
       setError("All fields are required");
     } else if (password !== confirmPassword) {
       setError("Passwords do not match");
+    } else if (!isAgeValid(dob)) {
+      setError("You must be at least 6 years old to register.");
     } else {
       //setIsRegistered(true);
       registerUser();  // Register the user in Firestore
     }
   };
 
+  const isAgeValid = (dateOfBirth) => {
+    const currentDate = new Date();
+    const birthDate = new Date(dateOfBirth);
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+      return age - 1 >= 6;
+    }
+
+    return age >= 6;
+  };
+  
   const registerUser = async () => {
     try {
       const userRef = collection(database, "users");
