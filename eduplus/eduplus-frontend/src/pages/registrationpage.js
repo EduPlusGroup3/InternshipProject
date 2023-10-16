02f71a57e194a05b130a41605e561f8255aef955
Ritik <mehta7b@uwindsor.ca>
2023-10-15 22:28:33 -0400

    Pulling3files

--- eduplus/eduplus-frontend/src/pages/registrationpage.js
+++ eduplus/eduplus-frontend/src/pages/registrationpage.js
@@ -1,4 +1,4 @@
-
+import "..//assests/styles/registrationpagestyles.css";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../assests/styles/registrationpagestyles.css"
