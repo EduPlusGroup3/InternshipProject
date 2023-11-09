d6812515fe2b4c995f55bb79bea0d495ccf96e53
Ritik <mehta7b@uwindsor.ca>
2023-11-09 10:25:28 -0500

    Firebase.js file update

--- eduplus/eduplus-frontend/src/firebase.js
+++ eduplus/eduplus-frontend/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 export const database = getFirestore(app);
+export { auth };
+export { app };
