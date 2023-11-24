127938c20ae254db4bbd4306a65e00beb4f44aec
viutikarathod@gmail.com <viutikarathod@gmail.com>
2023-11-23 20:26:02 -0500

    Video fix

--- eduplus/eduplus-frontend/src/components/VideoCarousel.js
+++ eduplus/eduplus-frontend/src/components/VideoCarousel.js
@@ -5,28 +5,28 @@ import Vid1 from "../assests/videos/vid1.mp4";
 import Vid2 from "../assests/videos/vid2.mp4";
 import Vid3 from "../assests/videos/vid3.mp4";
 import ReactPlayer from "react-player";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 
 const VideoCarousel = () => {
   const videoProperties = [
     {
       id: 1,
-      title: "Video 1",
+      title: "Demo Video",
       src: Vid1,
-      credit: "Video by cottonbro from Pexels",
+      
     },
-    {
-      id: 2,
-      title: "Video 2",
-      src: Vid2,
-      credit: "Video by cottonbro from Pexels",
-    },
-    {
-      id: 3,
-      title: "Video 3",
-      src: Vid3,
-      credit: "Video by cottonbro from Pexels",
-    },
+    // {
+    //   id: 2,
+    //   title: "Video 2",
+    //   src: Vid2,
+    //   credit: "Video by cottonbro from Pexels",
+    // },
+    // {
+    //   id: 3,
+    //   title: "Video 3",
+    //   src: Vid3,
+    //   credit: "Video by cottonbro from Pexels",
+    // },
   ];
 
   return (
